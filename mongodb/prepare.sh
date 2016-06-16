2a048679e54c56aef5fe7842e50957a1e1ef3b86
Vadim Tkachenko <vadim@percona.com>
2016-06-16 16:59:17 -0700

    Create prepare.sh

---
+++ mongodb/prepare.sh
@@ -0,0 +1,20 @@
+./sysbench \
+    --mongo-write-concern=1 \
+    --mongo-url="mongodb://localhost" \
+    --mongo-database-name=sbtest \
+    --test=tests/mongodb/oltp.lua \
+    --oltp_table_size=60000000 \
+    --oltp_tables_count=8 \
+    --num-threads=1 \
+    --rand-type=uniform \
+    --report-interval=10 \
+    --max-requests=0 \
+    --max-time=100000000 \
+    --oltp-point-selects=10 \
+    --oltp-simple-ranges=1 \
+    --oltp-sum-ranges=1 \
+    --oltp-order-ranges=1 \
+    --oltp-distinct-ranges=1 \
+    --oltp-index-updates=1 \
+    --oltp-non-index-updates=1 \
+    --oltp-inserts=1 prepare
