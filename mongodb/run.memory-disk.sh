6249d2960b870964828edb193a09d2871b1ae52c
Vadim Tkachenko <vadim@percona.com>
2016-06-18 10:43:10 -0700

    Update run.memory-disk.sh

--- mongodb/run.memory-disk.sh
+++ mongodb/run.memory-disk.sh
@@ -1,13 +1,13 @@
 source config-remote.sh
 DB="sbtest"
-DIR=res-OLTP-RW-memory-disk-sam863-8t-rocks
+DIR=res-OLTP-RO-thread-scale-sam863-8t-rocks
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 # cycle by buffer pool size
 
-for BP in `seq 4 8 200`
-#for BP in `seq 142 8 150`
+#for BP in `seq 4 8 200`
+for BP in 200
 do
 
 startmongo 
@@ -26,14 +26,16 @@ warmtime=600
 #for i in 1 2 4 6 10 106 25 38 56 82 120 175 250 360
 #for i in 4 6 10 16 25 38 56 82 120 175
 #for i in 56 82 120 
-for i in 14 28 56 84 112 140
+#for i in 14 28 56 84 112 140
+#for i in 112
+for i in 1 2 3 4 5 6 8 10 13 16 20 25 31 38 46 56 68 82 100 112 145 175 210 250 300 360 430 520 630 750 870 1000
 #for i in 14 
 do
 
         OUTDIR=$DIR/$runid/thr$i
         mkdir -p $OUTDIR
 
-        echo "{\"cache-size\":$BP,\"user-threads\":$i}\"" > $OUTDIR/runparam.json
+        echo "{\"cachesize\":$BP,\"userthreads\":$i}" > $OUTDIR/runparam.json
         echo "====== Running BP: $BP, Threads: $i ========"
 
         # start stats collection
@@ -41,7 +43,7 @@ do
         #collect_dstat_stats 
 
         time=300
-        ./sysbench --test=tests/mongodb/oltp.lua --oltp_tables_count=8 --oltp_table_size=60000000 --num-threads=$i --oltp-point-selects=10  --oltp-simple-ranges=1  --oltp-sum-ranges=1  --oltp-order-ranges=1  --oltp-distinct-ranges=1  --oltp-index-updates=1  --oltp-non-index-updates=1  --oltp-inserts=1 --oltp-read-only=on --max-time=$time --max-requests=0 --report-interval=10 --rand-type=uniform --rand-init=on --mongo-url="mongodb://$REMOTEHOST" --mongo-database-name=$DB  run | tee -a $OUTDIR/res.txt
+        ./sysbench --test=tests/mongodb/oltp.lua --oltp_tables_count=8 --oltp_table_size=60000000 --num-threads=$i --oltp-point-selects=10  --oltp-simple-ranges=1  --oltp-sum-ranges=1  --oltp-order-ranges=1  --oltp-distinct-ranges=1  --oltp-index-updates=0  --oltp-non-index-updates=0  --oltp-inserts=0 --oltp-read-only=on --max-time=$time --max-requests=0 --report-interval=10 --rand-type=uniform --rand-init=on --mongo-url="mongodb://$REMOTEHOST" --mongo-database-name=$DB  run | tee -a $OUTDIR/res.txt
 
         # kill stats
         set +e
