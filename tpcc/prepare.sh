fdb78f54e8a8256f20b954dfb4dfea9c4b01393a
Vadim Tkachenko <vadim@percona.com>
2018-03-17 12:54:34 -0700

    Create prepare.sh

---
+++ tpcc/prepare.sh
@@ -0,0 +1,2 @@
+/tpcc.lua --mysql-socket=/tmp/mysql.sock --mysql-user=root --mysql-db=sbrocks  --threads=32 --report-interval=1 --tables=10 --scale=100 --use_fk=0 --mysql_storage_engine=rocksdb --mysql_table_options="COLLATE latin1_bin" --trx_level=RC prepare
+/tpcc.lua --mysql-socket=/tmp/mysql.sock --mysql-user=root --mysql-db=sbinno  --threads=32 --report-interval=1 --tables=10 --scale=100 --use_fk=0 --mysql_storage_engine=innodb --mysql_table_options="COLLATE latin1_bin" --trx_level=RC prepare
